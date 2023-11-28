f0f254febb292c15aa5e29da71fd6c6fa79f9aed
Tejaswi281112 <152305169+Tejaswi281112@users.noreply.github.com>
2023-11-28 22:09:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing-3", {
+    var typed = new Typed(".typing-2", {
         strings: ["Programmer", "Web Developer", "Software Developer", "Designer", "Reader", "Music Lover"],
         typeSpeed: 100,
         backSpeed: 60,
